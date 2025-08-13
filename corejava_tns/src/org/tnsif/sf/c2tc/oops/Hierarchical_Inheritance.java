93436c226cf52350452dc0323bd3f4c6feab375f
deek <deek@de.org>
2025-08-13 09:19:44 +0530

    first commit

---
+++ corejava_tns/src/org/tnsif/sf/c2tc/oops/Hierarchical_Inheritance.java
@@ -0,0 +1,41 @@
+package org.tnsif.sf.c2tc.oops;
+
+
+class Employee
+{
+	void company()
+	{
+		System.out.println("Company :Tnsif");
+	}
+}
+class Developer extends Employee
+{
+	void role()
+	{
+		System.out.println("Role :Software Developer");
+		
+	}
+}
+class Tester extends Employee
+{
+	void role() {
+		System.out.println("Role :QA Tester");
+	}
+}
+
+
+public class Hierarchical_Inheritance {
+	public static void main(String[] args) {
+		Developer dev=new Developer();
+		Tester test=new Tester();
+		
+		dev.company();
+		dev.role();
+		
+		test.company();
+		test.role();
+
+	}
+
+
+}
